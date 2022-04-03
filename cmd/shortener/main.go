227a2451a404b7143843ece0b67248d2d9f38735
SavchenkoOleg <o.savchenko@procity.tech>
2022-04-03 17:07:13 +0500

    inc2

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,73 +1,20 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
+	"internal/handlers"
+	
 )
-
-var mapURL = make(map[string]string)
-var mapID = make(map[string]string)
-var newURLPref = "newURL"
-var localURL = "http://localhost:8080/"
-
-func handlerShot(w http.ResponseWriter, r *http.Request) {
-
-	var NewID string
-
-	b, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	bodyURL := string(b)
-
-	if len(bodyURL) == 0 {
-		http.Error(w, "uncorrect reduction URL", http.StatusBadRequest)
-		return
-	}
-
-	NewID, exp := mapURL[bodyURL]
-	if !exp {
-		NewID = newURLPref + strconv.Itoa(len(mapURL)+1)
-		mapURL[bodyURL] = NewID
-		mapID[NewID] = bodyURL
-	}
-	NewURL := localURL + NewID
-	w.WriteHeader(201)
-	w.Write([]byte(NewURL))
-}
-
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
-
-	idPath := r.URL.Path
-	if idPath == "" {
-		http.Error(w, "The parameter is missing", http.StatusBadRequest)
-		return
-	}
-	id := strings.TrimPrefix(idPath, "/")
-	url, exp := mapID[id]
-
-	if !exp {
-		http.Error(w, "URL for the specified id was not found", http.StatusBadRequest)
-		return
-	}
-
-	w.Header().Set("Location", url)
-	w.WriteHeader(http.StatusTemporaryRedirect)
-}
 
 func firstResort(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 
 	case http.MethodPost:
-		handlerShot(w, r)
+		handlers.HandlerShot(w, r)
 	case http.MethodGet:
-		handlerIndex(w, r)
+		handlers.HandlerIndex(w, r)
 	default:
 		http.Error(w, "Only POST or GET requests are allowed!", http.StatusMethodNotAllowed)
 	}
